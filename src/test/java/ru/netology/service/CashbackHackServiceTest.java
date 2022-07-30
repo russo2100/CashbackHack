2cebe03c3814b8ccbd82c0aec33ae6017dba388a
russo2100 <russolatypov@gmail.com>
2022-07-30 16:46:44 +0400

    commit messege

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,30 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void testRemain() {
+    public void remainTest1() {
+        CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int amount = 900;
+
         int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void testErrorRemain() {
+    public void remainTest2() {
         CashbackHackService service = new CashbackHackService();
-        int expected = 1100;
-        int amount = 0;
+        int expected = 0;
+        int amount = 1400;
 
         int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
